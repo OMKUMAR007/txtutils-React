588b6fb507ca007085c725678b36b39422d5b715
omk8461@gmail.com <omk8461@gmail.com>
2023-01-31 11:20:12 +0530

    Updates

---
+++ precache-manifest.fe95c79f06f6affb4c05fcb1b4bd7a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa8f80a14e3f597ac29",
+    "url": "/txtutils-React/static/css/main.bb3c565d.chunk.css"
+  },
+  {
+    "revision": "aaa8f80a14e3f597ac29",
+    "url": "/txtutils-React/static/js/main.aaa8f80a.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/txtutils-React/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/txtutils-React/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "2a5765cd83beff98fb9f",
+    "url": "/txtutils-React/static/js/runtime~main.2a5765cd.js"
+  },
+  {
+    "revision": "90d203d33833866ebe694a8078e74c25",
+    "url": "/txtutils-React/index.html"
+  }
+];
